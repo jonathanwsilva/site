03cbbb68528c3d39138e8c1f9ce7059c2a857e82
Jonathan Silva <jonathan.walker.silva@gmail.com>
2020-04-18 21:28:54 -0400

    Add an about section (The image just points to facebook's CDN for now)

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,15 @@ import theme from './about.module.css';
 
 export default () => (
     <Layout selected="About">
-        Hello world!
+        <div className={theme['about']}>
+            <h1>About Jon</h1>
+            <p>Hello! My name is Jonathan. Most people call me Jon.</p>
+            <img alt="Jon sits near a banyan tree" src='https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/82384197_2870603016317175_3793107338194321408_n.jpg?_nc_cat=107&_nc_sid=110474&_nc_oc=AQlM1uaOnrcwDRN0ng6VrsoXGKp2L8qr9f6mj70z5g4O7bTx7QIyytgZmHYVy40cjWA&_nc_ht=scontent-lga3-1.xx&oh=f9dc258ec2f9f33a4521eb23f8d7baa0&oe=5EBFE209'></img>
+            <div>
+                <p>I'm a software engineering co-op at Curriculum Associates Inc. and a senior Computer Science student at UMass Lowell.</p>
+                <p>I'm interested in functional programming, distributed systems, and web development. I'm also big on gardening.</p>
+                <p>In the past I have studied Chemical Engineering and worked as a controls engineer in the biotech industry.</p>
+            </div>
+        </div>
     </Layout>
 )
