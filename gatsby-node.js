db35d48425fa60df529581ac6644ac48bc36b865
Jonathan Silva <jonathan.walker.silva@gmail.com>
2020-04-25 13:46:37 -0400

    Create a page per markdown file

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,3 +11,27 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     })
   }
 }
+
+exports.createPages = async function ({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  data.allMarkdownRemark.edges.forEach(edge => {
+    const {slug} = edge.node.fields
+    actions.createPage({
+      path: slug,
+      component: require.resolve(`./src/templates/project-post.js`),
+      context: { slug: slug },
+    })
+  })
+}
